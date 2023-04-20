235c060cfce3a72b467434f81e827a92d99f2dba
Dmitriy Angve <kirirch129@gmail.com>
2023-04-20 13:48:21 +0300

    Anagrama task

--- script.js
+++ script.js
@@ -966,3 +966,18 @@ function stray(numbers) {
 
 console.log(stray([1, 0, 1, 1]));
 */
+
+// 20.04.2023
+// let first = test.split("");
+// let second = original.split("");
+// console.log(first);
+// console.log(second);
+// return first === second;
+var isAnagram = function (test, original) {
+  return (
+    test.toLowerCase().split("").sort().join() ===
+    original.toLowerCase().split("").sort().join()
+  );
+};
+
+console.log(isAnagram("Twoo", "WooT"));
