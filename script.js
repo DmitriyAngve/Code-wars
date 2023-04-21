406e28860d7c27b3d7cd297e7328fe7ae524d947
Dmitriy Angve <kirirch129@gmail.com>
2023-04-21 11:33:35 +0300

    break up camel case

--- script.js
+++ script.js
@@ -1077,17 +1077,43 @@ Constraint:
     1 <= month <= 12
 
 */
-const quarterOf = (month) => {
-  if (month <= 3) {
-    return 1;
-  }
-  if (month >= 4 && month <= 6) {
-    return 2;
-  }
-  if (month >= 7 && month <= 9) {
-    return 3;
-  }
-  if (month >= 10 && month <= 12) {
-    return 4;
-  }
-};
+// const quarterOf = (month) => {
+//   if (month <= 3) {
+//     return 1;
+//   }
+//   if (month >= 4 && month <= 6) {
+//     return 2;
+//   }
+//   if (month >= 7 && month <= 9) {
+//     return 3;
+//   }
+//   if (month >= 10 && month <= 12) {
+//     return 4;
+//   }
+// };
+
+// #4
+/*
+Complete the solution so that the function will break up camel casing, using a space between words.
+Example
+
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+*/
+// function solution(string) {
+//   let str = string.split("").join();
+//   for (let i = 0; i < str.length; i++) {
+//     if (str.charAt(i).toUpperCase() === str.charAt(i)) {
+//       return str.replace(i, " ");
+//     }
+//   }
+//   return str.join();
+// }
+
+// function solution(string) {
+//   const newStr = string.replace(/([A-Z])/g, " $1");
+//   return newStr;
+// }
+
+// console.log(solution("camelCasing"));
