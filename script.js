0f96f8a6035ba1248df3bb3db2a3d6761ec23603
Dmitriy Angve <kirirch129@gmail.com>
2023-04-21 11:20:12 +0300

    if conditions

--- script.js
+++ script.js
@@ -1053,15 +1053,41 @@ Don't forget to convert the percent parameter as a percentage in the body of you
 
 // #2
 // Write function bmi that calculates body mass index (bmi = weight / height2).
-function bmi(weight, height) {
-  let bmi = weight / Math.pow(height, 2);
-  return bmi <= 18.5
-    ? "Underweight"
-    : bmi <= 25
-    ? "Normal"
-    : bmi <= 30
-    ? "Overweight"
-    : "Obese";
-}
+// function bmi(weight, height) {
+//   let bmi = weight / Math.pow(height, 2);
+//   return bmi <= 18.5
+//     ? "Underweight"
+//     : bmi <= 25
+//     ? "Normal"
+//     : bmi <= 30
+//     ? "Overweight"
+//     : "Obese";
+// }
 
-console.log(bmi(100, 1.87));
+// console.log(bmi(100, 1.87));
+
+// #3
+/*
+Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+Constraint:
+
+    1 <= month <= 12
+
+*/
+const quarterOf = (month) => {
+  if (month <= 3) {
+    return 1;
+  }
+  if (month >= 4 && month <= 6) {
+    return 2;
+  }
+  if (month >= 7 && month <= 9) {
+    return 3;
+  }
+  if (month >= 10 && month <= 12) {
+    return 4;
+  }
+};
