e731257bb60471bf1020ffc39a206c1d78fb5142
Dmitriy Angve <kirirch129@gmail.com>
2023-04-24 08:51:23 +0300

    if conditions

--- script.js
+++ script.js
@@ -1221,6 +1221,7 @@ For example, if the parameters passed are (2, 6), the function should return [2,
 //     }
 //   }
 // }
+/*
 function findMultiples(integer, limit) {
   let newArr = [];
   for (let i = integer; i <= limit; i++) {
@@ -1231,3 +1232,40 @@ function findMultiples(integer, limit) {
   return newArr;
 }
 console.log(findMultiples(2, 6));
+*/
+
+// #5
+/*
+Grade book
+
+Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+Numerical Score 	Letter Grade
+90 <= score <= 100 	'A'
+80 <= score < 90 	'B'
+70 <= score < 80 	'C'
+60 <= score < 70 	'D'
+0 <= score < 60 	'F'
+
+Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+*/
+function getGrade(s1, s2, s3) {
+  let score = (s1 + s2 + s3) / 3;
+  console.log(score);
+  if (score >= 90 && score <= 100) {
+    return "A";
+  }
+  if (score >= 80 && score < 90) {
+    return "B";
+  }
+  if (score >= 70 && score < 80) {
+    return "C";
+  }
+  if (score >= 60 && score < 70) {
+    return "D";
+  }
+  if (score >= 0 && score < 60) {
+    return "F";
+  }
+}
+
+console.log(getGrade(89, 89, 90));
