f9dde3ca38d15c2604ace7e48fc4fb8d0c777242
Dmitriy Angve <kirirch129@gmail.com>
2023-04-24 08:32:17 +0300

    arr

--- script.js
+++ script.js
@@ -1195,6 +1195,7 @@ b = 4
 --> [1, 2, 3, 4]
 
 */
+/*
 function between(a, b) {
   let arr = [];
   for (let i = a; i <= b; i++) {
@@ -1203,3 +1204,30 @@ function between(a, b) {
   return arr;
 }
 console.log(between(1, 4));
+*/
+
+// #4
+/*
+In this simple exercise, you will build a program that takes a value, integer , and returns a list of its multiples up to another value, limit . If limit is a multiple of integer, it should be included as well. There will only ever be positive integers passed into the function, not consisting of 0. The limit will always be higher than the base.
+
+For example, if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.
+*/
+
+// function findMultiples(integer, limit) {
+//   let newArr = [];
+//   if (limit % integer === 0) {
+//     for (let i = integer; i < limit; i++) {
+
+//     }
+//   }
+// }
+function findMultiples(integer, limit) {
+  let newArr = [];
+  for (let i = integer; i <= limit; i++) {
+    if ([i] % integer === 0) {
+      newArr.push(i);
+    }
+  }
+  return newArr;
+}
+console.log(findMultiples(2, 6));
