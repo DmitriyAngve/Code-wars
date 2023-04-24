15d839a595ddbf0ba5da24e490a88941e1a1d19f
Dmitriy Angve <kirirch129@gmail.com>
2023-04-24 09:44:57 +0300

    index

--- script.js
+++ script.js
@@ -1286,6 +1286,7 @@ String will never be empty and you do not need to account for different data typ
 
 
 */
+/*
 function findShort(s) {
   let words = s.split(" ");
   const short = words.reduce(
@@ -1296,3 +1297,20 @@ function findShort(s) {
 }
 
 console.log(findShort("bitcoin take over the world maybe who knows perhaps"));
+*/
+
+// # 7
+
+/*
+You are given an array with positive numbers and a non-negative number N. You should find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0.
+
+Let's look at a few examples:
+
+    array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
+    array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
+*/
+function index(array, n) {
+  return array[n] ** n || -1;
+}
+
+console.log(index([1, 3, 10, 100], 3));
