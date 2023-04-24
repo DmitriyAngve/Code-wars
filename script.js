f08b152de40a8faf156abdd9c2337ac5b82d0713
Dmitriy Angve <kirirch129@gmail.com>
2023-04-24 07:40:24 +0300

    arr between integers

--- script.js
+++ script.js
@@ -1183,3 +1183,23 @@ function duplicateCount(text){
   return (text.toLowerCase().split('').sort().join('').match(/([^])\1+/g) || []).length;
 }
 */
+
+// #3
+/*
+Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+For example:
+
+a = 1
+b = 4
+--> [1, 2, 3, 4]
+
+*/
+function between(a, b) {
+  let arr = [];
+  for (let i = a; i <= b; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
+console.log(between(1, 4));
