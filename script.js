7e2b33fbf151b1c216579968ac70ea22afc8462b
Dmitriy Angve <kirirch129@gmail.com>
2023-04-25 08:31:19 +0300

    to decimal

--- script.js
+++ script.js
@@ -1372,15 +1372,36 @@ Examples
 "www.codewars.com#about" --> "www.codewars.com"
 "www.codewars.com?page=1" -->"www.codewars.com?page=1"
 */
-
+/*
 function removeUrlAnchor(url) {
   let newStr = url.split("#")[0];
   return newStr;
 }
 // string.replace(/то, что меняем/g, "на что меняем")
 console.log(removeUrlAnchor("www.codewars.com#about"));
-
+*/
 // Second solution
-function removeUrlAnchor(url) {
-  return url.replace(/#.*/gi, "");
+// function removeUrlAnchor(url) {
+//   return url.replace(/#.*/gi, "");
+// }
+
+// #5
+// Binary Addition
+/*
+Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+The binary number returned should be a string.
+
+Examples:(Input1, Input2 --> Output (explanation)))
+
+1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+*/
+/*
+function addBinary(a, b) {
+  let sum = (a + b).toString(2);
+  return sum;
 }
+
+console.log(addBinary(1, 1));
+*/
