c0b695c09e83953ec63ae24f6ee80a3cb040e79c
Dmitriy Angve <kirirch129@gmail.com>
2023-04-26 07:24:35 +0300

    minMax

--- script.js
+++ script.js
@@ -1558,6 +1558,7 @@ remove("!Hi!") == "!Hi"
 remove("Hi! Hi!") == "Hi! Hi"
 remove("Hi") == "Hi"
  */
+/*
 function remove(str) {
   if (str.charAt(str.length - 1) === "!") {
     return str.slice(0, -1);
@@ -1567,3 +1568,40 @@ function remove(str) {
 
 // const remove = s => s.replace(/!$/, '');
 console.log(remove("!Hi!"));
+*/
+
+// 26.04.2022
+
+// #1
+/*
+Task
+    Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+    In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained (Read the notes for more detail about it)
+Example
+With the numbers are 1, 2 and 3 , here are some ways of placing signs and brackets:
+    1 * (2 + 3) = 5
+    1 * 2 * 3 = 6
+    1 + 2 * 3 = 7
+    (1 + 2) * 3 = 9
+So the maximum value that you can obtain is 9.
+Notes
+    The numbers are always positive.
+    The numbers are in the range (1  ≤  a, b, c  ≤  10).
+    You can use the same operation more than once.
+    It's not necessary to place all the signs and brackets.
+    Repetition in numbers may occur .
+    You cannot swap the operands. For instance, in the given example you cannot get expression (1 + 3) * 2 = 8
+*/
+function expressionMatter(a, b, c) {
+  const expressions = [
+    a + b + c,
+    a * b * c,
+    (a + b) * c,
+    a * (b + c),
+    a + b * c,
+    a * b + c,
+  ];
+  return Math.max(...expressions);
+}
+
+console.log(expressionMatter(2, 1, 2));
