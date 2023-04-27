98e06972264525406d43533f68f116568a0cfdce
Dmitriy Angve <kirirch129@gmail.com>
2023-04-27 09:41:04 +0300

    mathceil

--- script.js
+++ script.js
@@ -1969,6 +1969,7 @@ uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 uniqueInOrder([1,2,2,3,3])       == [1,2,3]
 */
+/*
 function uniqueInOrder(iterable) {
   let result = [];
 
@@ -1983,3 +1984,27 @@ function uniqueInOrder(iterable) {
 
 console.log(uniqueInOrder([1, 2, 2, 3, 3]));
 console.log(uniqueInOrder("AAAABBBCCDAABBB"));
+*/
+
+// #8
+/*
+Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+Examples:
+input:    output:
+0    ->   0
+2    ->   5
+3    ->   5
+12   ->   15
+21   ->   25
+30   ->   30
+-2   ->   0
+-5   ->   -5
+etc.
+Input may be any positive or negative integer (including 0).
+You can assume that all inputs are valid integers.
+*/
+function roundToNext5(n) {
+  let str = Math.ceil(n / 5) * 5;
+  return str;
+}
+console.log(roundToNext5(2));
