50c843834f18e51462cc93774109ec7167ed1333
Dmitriy Angve <kirirch129@gmail.com>
2023-04-27 07:37:46 +0300

    multable

--- script.js
+++ script.js
@@ -1838,7 +1838,7 @@ If there are more than 6 petals, you start over with "I love you" for 7 petals, 
 When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
 Your goal in this kata is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. The number of petals is always greater than 0.
  */
-
+/*
 function howMuchILoveYou(nbPetals) {
   const nbArr = [
     "I love you",
@@ -1853,3 +1853,32 @@ function howMuchILoveYou(nbPetals) {
 }
 
 console.log(howMuchILoveYou(7));
+*/
+
+// #2
+
+/*
+Your goal is to return multiplication table for number that is always an integer from 1 to 10.
+For example, a multiplication table (string) for number == 5 looks like below:
+1 * 5 = 5
+2 * 5 = 10
+3 * 5 = 15
+4 * 5 = 20
+5 * 5 = 25
+6 * 5 = 30
+7 * 5 = 35
+8 * 5 = 40
+9 * 5 = 45
+10 * 5 = 50
+P. S. You can use \n in string to jump to the next line.
+Note: newlines should be added between rows, but there should be no trailing newline at the end. If you're unsure about the format, look at the sample tests.
+*/
+
+function multiTable(number) {
+  let str = "";
+  for (let i = 1; i <= 10; i++) {
+    str += `${i} * ${number} = ${i * number}\n`;
+  }
+  return str.trim();
+}
+console.log(multiTable(5));
