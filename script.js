6c105c5c33823567aa552e51b3d5559441a7d07c
Dmitriy Angve <kirirch129@gmail.com>
2023-04-28 09:25:23 +0300

    ASCII

--- script.js
+++ script.js
@@ -2243,6 +2243,7 @@ Examples: (Input --> Output)
 20 --> "drink beer"
 30 --> "drink whisky"
 */
+/*
 function peopleWithAgeDrink(old) {
   return old < 14
     ? "drink toddy"
@@ -2259,3 +2260,24 @@ function peopleWithAgeDrink(old) {
 // }
 
 console.log(peopleWithAgeDrink(22));
+*/
+/*
+function binToDec(bin) {
+  return parseInt(bin, 2);
+}
+*/
+
+// #9
+
+/*
+Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+Example:
+get_char(65)
+should return:
+'A'
+*/
+function getChar(c) {
+  return String.fromCharCode(c);
+}
+
+console.log(getChar(55));
