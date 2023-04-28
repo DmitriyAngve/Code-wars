3bc26a7c887426db5e9187a111b3808712d367fe
Dmitriy Angve <kirirch129@gmail.com>
2023-04-28 07:36:42 +0300

    reduce

--- script.js
+++ script.js
@@ -2038,6 +2038,8 @@ console.log(century(89));
 The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
 What if the string is empty? Then the result should be empty object literal, {}.
 */
+/*
+
 function countChars(str) {
   let counts = {};
   for (let i = 0; i < str.length; i++) {
@@ -2051,3 +2053,30 @@ function countChars(str) {
   return counts;
 }
 console.log(countChars("aba"));
+*/
+
+// 28.04
+
+// #1
+/*
+Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+Examples
+Input: [1, 5.2, 4, 0, -1]
+Output: 9.2
+Input: []
+Output: 0
+Input: [-2.398]
+Output: -2.398
+Assumptions
+    You can assume that you are only given numbers.
+    You cannot assume the size of the array.
+    You can assume that you do get an array and if the array is empty, return 0.
+What We're Testing
+We're testing basic loops and math operations. This is for beginners who are just learning loops and math operations.
+Advanced users may find this extremely easy and can easily write this in one line.
+*/
+function sum(numbers) {
+  return numbers.reduce((acc, val) => acc + val, 0);
+}
+
+console.log(sum([1, 5.2, 4, 0, -1]));
