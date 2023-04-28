9ed4d7dd05bfcde5ad98b41c43c0fd3742d6463c
Dmitriy Angve <kirirch129@gmail.com>
2023-04-28 09:38:06 +0300

    avg

--- script.js
+++ script.js
@@ -2276,8 +2276,28 @@ get_char(65)
 should return:
 'A'
 */
+/*
 function getChar(c) {
   return String.fromCharCode(c);
 }
 
 console.log(getChar(55));
+*/
+
+// #10
+
+/*
+There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+Return True if you're better, else False!
+Note:
+Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+*/
+function betterThanAverage(classPoints, yourPoints) {
+  const sum = classPoints.reduce((a, b) => a + b, 0 / classPoints.length);
+  const avg = (sum + yourPoints) / (classPoints.length + 1);
+  return yourPoints > avg;
+}
+
+console.log(betterThanAverage([41, 75, 72, 56, 80, 82, 81, 33], 50));
