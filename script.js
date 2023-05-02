3558ed982ab7b2c559c94ac674276eed686a0a03
Dmitriy Angve <kirirch129@gmail.com>
2023-05-02 07:32:20 +0300

    matches

--- script.js
+++ script.js
@@ -2333,6 +2333,7 @@ Example(Input --> Output)
 Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element.
 Note: String will have at least one element; words will always be separated by a space.
 */
+/*
 function addLength(str) {
   let newStr = str.split(" ");
   let newArr = [];
@@ -2344,9 +2345,36 @@ function addLength(str) {
   }
   return newArr;
 }
-/*
-function addLength(str){
-  return str.split(" ").map(s => `${s} ${s.length}`)
-}
-*/
+
+// function addLength(str){
+//   return str.split(" ").map(s => `${s} ${s.length}`)
+// }
+
 console.log(addLength("you will win"));
+*/
+
+// #2
+
+/*
+In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+The string has a length greater or equal to one and contains only letters from ato z.
+Examples:
+s="aaabbbbhaijjjm"
+printer_error(s) => "0/14"
+s="aaaxbbbbyyhwawiwjjjwwm"
+printer_error(s) => "8/22"
+*/
+function printerError(s) {
+  const regex = /[a-m]/gi;
+  const matches = s.match(regex);
+  const errors = s.length - matches.length;
+  const result = `${errors}/${s.length}`;
+  return result;
+}
+
+console.log(
+  printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz")
+);
