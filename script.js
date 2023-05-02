19c4dc749895f59ebe6759a731c364e9a7b4c510
Dmitriy Angve <kirirch129@gmail.com>
2023-05-02 08:38:46 +0300

    newReg

--- script.js
+++ script.js
@@ -2411,6 +2411,7 @@ expandedForm(70304); // Should return '70000 + 300 + 4'
 NOTE: All numbers will be whole numbers greater than 0.
 If you liked this kata, check out part 2!!
 */
+/*
 function expandedForm(num) {
   let digits = String(num).split("");
   let result = [];
@@ -2433,3 +2434,27 @@ function expandedForm(num) {
 //                             .reverse()
 //                             .join(" + ");
 console.log(expandedForm(70333));
+
+*/
+// #5
+/*
+Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+If no occurrences can be found, a count of 0 should be returned.
+("Hello", "o")  ==>  1
+("Hello", "l")  ==>  2
+("", "z")       ==>  0
+str_count("Hello", 'o'); // returns 1
+str_count("Hello", 'l'); // returns 2
+str_count("", 'z'); // returns 0
+Notes
+    The first argument can be an empty string
+    In languages with no distinct character data type, the second argument will be a string of length 1
+*/
+
+function strCount(str, letter) {
+  const regex = new RegExp(letter, "g");
+  const matches = str.match(regex);
+  return matches ? matches.length : 0;
+}
+
+console.log(strCount("", "z"));
