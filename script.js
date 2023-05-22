96adc0a3a33c5b3ea31cd1318240534fbdfb9772
Dmitriy Angve <kirirch129@gmail.com>
2023-05-22 08:49:58 +0300

    wavw

--- script.js
+++ script.js
@@ -2539,9 +2539,35 @@ Testing: [1, 0, 1, 1] ==> 11
 However, the arrays can have varying lengths, not just limited to 4.
 
 */
+/*
 function binaryArrayToNumber(arr) {
   let decimal = arr.join("");
   return parseInt(decimal, 2);
 }
 
 console.log(binaryArrayToNumber([0, 0, 0, 1]));
+*/
+
+// #3
+/*
+Task
+In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up. 
+Rules
+ 1.  The input string will always be lower case but maybe empty.
+ 2.  If the character in the string is whitespace then pass over it as if it was an empty seat
+Example
+wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+ */
+function wave(str) {
+  let result = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === " ") {
+      continue;
+    }
+    let waveStr = str.slice(0, i) + str[i].toUpperCase() + str.slice(i + 1);
+    result.push(waveStr);
+  }
+  return result;
+}
+
+console.log(wave("hello"));
