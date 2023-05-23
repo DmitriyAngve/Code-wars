ac299014691d5c99e1ad58c46e8e20abfc369a72
Dmitriy Angve <kirirch129@gmail.com>
2023-05-23 11:57:35 +0300

    factorial

--- script.js
+++ script.js
@@ -2648,6 +2648,7 @@ Examples
 1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
 1,5,3  --> 5 (1 + 4)
 */
+/*
 const sequenceSum = (begin, end, step) => {
   if (begin > end) {
     return 0;
@@ -2660,3 +2661,30 @@ const sequenceSum = (begin, end, step) => {
 };
 
 console.log(sequenceSum(1, 5, 1));
+*/
+
+// #4
+
+/*
+In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
+Write a function to calculate factorial for a given input. If input is below 0 or above 12 throw an exception of type throw a RangeError (JavaScript).
+More details about factorial can be found here.
+*/
+function factorial(n) {
+  if (n < 0 || n > 12) {
+    throw new RangeError("Input is out of range.");
+  }
+
+  if (n === 0) {
+    return 1;
+  }
+
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+
+  return result;
+}
+
+console.log(factorial(5));
