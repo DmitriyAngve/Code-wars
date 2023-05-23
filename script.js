664d20b5500e1007bac6882b3b94d68d8b8fc5a5
Dmitriy Angve <kirirch129@gmail.com>
2023-05-23 11:21:12 +0300

    sliceN

--- script.js
+++ script.js
@@ -2581,9 +2581,23 @@ Define a function that removes duplicates from an array of numbers and returns i
 The order of the sequence has to stay the same.
 
 */
+/*
 function distinct(a) {
   let set = new Set(a);
   return [...set];
 }
 
 console.log(distinct([1, 1, 2]));
+*/
+
+// 23.05
+
+// #1
+/*
+Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+*/
+function take(arr, n) {
+  return arr.slice(0, n);
+}
+
+console.log(take([0, 1, 2, 3, 5, 8, 13], 3));
