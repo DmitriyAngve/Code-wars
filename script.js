783be955ee49c4da278ef819a2c7bd8ea55158f8
Dmitriy Angve <kirirch129@gmail.com>
2023-05-23 11:37:47 +0300

    eurika

--- script.js
+++ script.js
@@ -2596,8 +2596,40 @@ console.log(distinct([1, 1, 2]));
 /*
 Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
 */
+/*
 function take(arr, n) {
   return arr.slice(0, n);
 }
 
 console.log(take([0, 1, 2, 3, 5, 8, 13], 3));
+*/
+
+// #2
+/*
+The number 898989 is the first integer with more than one digit that fulfills the property partially introduced in the title of this kata. What's the use of saying "Eureka"? Because this sum gives the same number: 89=81+9289 = 8^1 + 9^289=81+92
+The next number in having this property is 135135135:
+See this property again: 135=11+32+53135 = 1^1 + 3^2 + 5^3135=11+32+53
+Task
+We need a function to collect these numbers, that may receive two integers aaa, bbb that defines the range [a,b][a, b][a,b] (inclusive) and outputs a list of the sorted numbers in the range that fulfills the property described above.
+Examples
+Let's see some cases (input -> output):
+1, 10  --> [1, 2, 3, 4, 5, 6, 7, 8, 9]
+1, 100 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+*/
+function sumDigPow(a, b) {
+  let result = [];
+  for (let num = a; num <= b; num++) {
+    let dig = num.toString().slice("");
+    let sum = 0;
+
+    for (let i = 0; i < dig.length; i++) {
+      sum += Math.pow(parseInt(dig[i]), i + 1);
+    }
+    if (sum === num) {
+      result.push(num);
+    }
+  }
+  return result;
+}
+
+console.log(sumDigPow(1, 100));
