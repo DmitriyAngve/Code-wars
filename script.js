8c7affdd8ac3457123e121f2bfa309fdcb38df48
Dmitriy Angve <kirirch129@gmail.com>
2023-06-05 15:01:16 +0300

    oddOrEven

--- script.js
+++ script.js
@@ -3078,6 +3078,7 @@ Examples(Operator, value1, value2) --> output
 ('*', 5, 5) --> 25
 ('/', 49, 7) --> 7
 */
+/*
 function basicOp(operation, value1, value2) {
   switch (operation) {
     case "+":
@@ -3092,3 +3093,28 @@ function basicOp(operation, value1, value2) {
 }
 
 console.log(basicOp("+", 4, 7));
+*/
+
+// #15
+/*
+Given a list of integers, determine whether the sum of its elements is odd or even.
+Give your answer as a string matching "odd" or "even".
+If the input array is empty consider it as: [0] (array with a zero).
+Examples:
+Input: [0]
+Output: "even"
+Input: [0, 1, 4]
+Output: "odd"
+Input: [0, -1, -5]
+Output: "even"
+Have fun!
+*/
+function oddOrEven(array) {
+  if (array.length === 0) {
+    return "even";
+  } else {
+    return array.reduce((acc, el) => acc + el, 0) % 2 === 0 ? "even" : "odd";
+  }
+}
+
+console.log(oddOrEven([0]));
