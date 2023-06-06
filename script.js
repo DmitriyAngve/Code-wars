4904ae965f1f4c81b2a12072281c75462cde25d6
Dmitriy Angve <kirirch129@gmail.com>
2023-06-06 14:43:02 +0300

    nearestSq

--- script.js
+++ script.js
@@ -3171,7 +3171,6 @@ function findNextSquare(sq) {
 }
 
 console.log(findNextSquare(114));
-
 */
 
 // #19
@@ -3380,5 +3379,52 @@ console.log(smash(["hello", "world", "this", "is", "great"]));
 
 // #9
 /*
+Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+Examples (Input -> Output)
+* [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+* [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+* [42, 54, 65, 87, 0]             -> min = 0, max = 87
+* [5]                             -> min = 5, max = 5
+*/
+/*
+var min = function (list) {
+  return Math.min(...list);
+};
 
+var max = function (list) {
+  return Math.max(...list);
+};
+*/
+
+// #10
+/*
+Your task is to find the nearest square number, nearest_sq(n) or nearestSq(n), of a positive integer n.
+For example, if n = 111, then nearest\_sq(n) (nearestSq(n)) equals 121, since 111 is closer to 121, the square of 11, than 100, the square of 10.
+If the n is already the perfect square (e.g. n = 144, n = 81, etc.), you need to just return n.
+*/
+function nearestSq(n) {
+  const sqrt = Math.sqrt(n);
+  const lowSqrt = Math.floor(sqrt) ** 2;
+  const highSqrt = Math.ceil(sqrt) ** 2;
+
+  if (n - lowSqrt <= highSqrt - n) {
+    return lowSqrt;
+  } else {
+    return highSqrt;
+  }
+}
+
+console.log(nearestSq(111));
+console.log(nearestSq(100));
+/*
+function findNextSquare(sq) {
+  let result = Math.pow(Math.sqrt(sq) + 1, 2);
+  if (Number.isInteger(result)) {
+    return result;
+  } else {
+    return -1;
+  }
+}
+
+console.log(findNextSquare(114));
 */
