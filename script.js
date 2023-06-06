ac99751fc73be535c2d1cee6cc57b748d602116e
Dmitriy Angve <kirirch129@gmail.com>
2023-06-06 13:32:42 +0300

    switchCaseTraining

--- script.js
+++ script.js
@@ -3302,8 +3302,47 @@ You will be given the high street price (normPrice), the duty free discount (dis
 For example, if a bottle cost £10 normally and the discount in duty free was 10%, you would save £1 per bottle. If your holiday cost £500, the answer you should return would be 500.
 All inputs will be integers. Please return an integer. Round down.
 */
+/*
 function dutyFree(normPrice, discount, hol) {
   return Math.floor(hol / (normPrice * (discount / 100)));
 }
 
 console.log(dutyFree(12, 50, 1000));
+*/
+
+// #6
+/*
+Complete the function which returns the weekday according to the input number:
+    1 returns "Sunday"
+    2 returns "Monday"
+    3 returns "Tuesday"
+    4 returns "Wednesday"
+    5 returns "Thursday"
+    6 returns "Friday"
+    7 returns "Saturday"
+    Otherwise returns "Wrong, please enter a number between 1 and 7"
+*/
+function whatday(num) {
+  if (num < 1 || num > 7) {
+    return "Wrong, please enter a number between 1 and 7";
+  } else {
+    switch (num) {
+      case 1:
+        return "Sunday";
+      case 2:
+        return "Monday";
+      case 3:
+        return "Tuesday";
+      case 4:
+        return "Wednesday";
+      case 5:
+        return "Thursday";
+      case 6:
+        return "Friday";
+      case 7:
+        return "Saturday";
+    }
+  }
+}
+
+console.log(whatday(1));
