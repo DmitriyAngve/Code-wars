d85f40a95c2d131746689ae6ba9b4eef201f7aaa
Dmitriy Angve <kirirch129@gmail.com>
2023-06-06 13:25:56 +0300

    smallTasks

--- script.js
+++ script.js
@@ -3261,6 +3261,7 @@ console.log(
 ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
 Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
 */
+/*
 function rot13(message) {
   let arr = message.split("");
   let rot = arr.map((char) => {
@@ -3277,3 +3278,32 @@ function rot13(message) {
 }
 
 console.log(rot13("friends"));
+*/
+
+// #4
+/*
+Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+Example:    
+5.5589 is rounded 5.56   
+3.3424 is rounded 3.34
+*/
+/*
+function twoDecimalPlaces(num) {
+  return Math.round(num * 100) / 100;
+}
+
+console.log(twoDecimalPlaces(110.09));
+*/
+
+// #5
+/*
+The purpose of this kata is to work out just how many bottles of duty free whiskey you would have to buy such that the saving over the normal high street price would effectively cover the cost of your holiday.
+You will be given the high street price (normPrice), the duty free discount (discount) and the cost of the holiday.
+For example, if a bottle cost £10 normally and the discount in duty free was 10%, you would save £1 per bottle. If your holiday cost £500, the answer you should return would be 500.
+All inputs will be integers. Please return an integer. Round down.
+*/
+function dutyFree(normPrice, discount, hol) {
+  return Math.floor(hol / (normPrice * (discount / 100)));
+}
+
+console.log(dutyFree(12, 50, 1000));
