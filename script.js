688e2bc1fe66912ab7edb8a5b1d8cf727b10ffb0
Dmitriy Angve <kirirch129@gmail.com>
2023-06-07 17:12:53 +0300

    Facebooklikes

--- script.js
+++ script.js
@@ -3712,6 +3712,80 @@ function evenNumbers(array, number) {
 console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
 */
 // SEC SOL
+/*
 const evenNumbers = (array, number) =>
   array.filter((item) => item % 2 === 0).slice(-number);
 console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
+*/
+
+// #7
+/*
+Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+*/
+/*
+function problem(x) {
+  return typeof x === "number" ? x * 50 + 6 : "Error";
+}
+
+console.log(problem("hello"));
+*/
+
+// #8
+/*
+Complete the function which converts hex number (given as a string) to a decimal number.
+*/
+/*
+function hexToDec(hexString) {
+  return parseInt(hexString, 16);
+}
+
+console.log(hexToDec("a"));
+*/
+
+// #9
+/*
+You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+Note: For 4 or more names, the number in "and 2 others" simply increases.
+*/
+function likes(names) {
+  let length = names.length;
+
+  switch (length) {
+    case 0:
+      return "no one likes this";
+    case 1:
+      return `${names[0]} likes this`;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default:
+      return `${names[0]}, ${names[1]} and ${length - 2} others like this`;
+  }
+}
+
+console.log(likes(["Alex", "Jacob", "Mark", "Max"]));
+
+/*
+function calculator(a, b, sign) {
+  if (typeof a === "number" && typeof b === "number") {
+    switch (sign) {
+      case "+":
+        return a + b;
+      case "-":
+        return a - b;
+      case "/":
+        return a / b;
+      case "*":
+        return a * b;
+    }
+  }
+  return "unknown value";
+} 
+*/
