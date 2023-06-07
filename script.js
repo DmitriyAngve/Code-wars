11d60bb992ae5047fc3d8e56527ddfa85c0ce423
Dmitriy Angve <kirirch129@gmail.com>
2023-06-07 16:20:43 +0300

    countSmileys

--- script.js
+++ script.js
@@ -3620,7 +3620,7 @@ calculator(1,2,"+"); //=> result will be 3
 calculator(1,2,"&"); //=> result will be "unknown value"
 calculator(1,"k","*"); //=> result will be "unknown value"
 */
-
+/*
 function calculator(a, b, sign) {
   if (typeof a === "number" && typeof b === "number") {
     switch (sign) {
@@ -3638,3 +3638,49 @@ function calculator(a, b, sign) {
 }
 
 console.log(calculator(1, 2, "+"));
+*/
+
+// #5
+/*
+Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+Rules for a smiling face:
+    Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+    A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+    Every smiling face must have a smiling mouth that should be marked with either ) or D
+No additional characters are allowed except for those mentioned.
+Valid smiley face examples: :) :D ;-D :~)
+Invalid smiley faces: ;( :> :} :]
+Example
+countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+Note
+In case of an empty array return 0. You will not be tested with inval
+*/
+
+function countSmileys(arr) {
+  const validSmileys = [
+    ":)",
+    ":D",
+    ";)",
+    ";D",
+    ":-)",
+    ":-D",
+    ";-)",
+    ";-D",
+    ":~)",
+    ":~D",
+    ";~)",
+    ";~D",
+  ];
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (validSmileys.includes(arr[i])) {
+      count++;
+    }
+  }
+
+  return count;
+}
+console.log(countSmileys([":D", ":~)", ";~D", ":)"]));
