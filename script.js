687f541d449a87592f6d7945efdc66c3221061fc
Dmitriy Angve <kirirch129@gmail.com>
2023-06-07 15:19:28 +0300

    vowelIndices

--- script.js
+++ script.js
@@ -3566,6 +3566,7 @@ Write a function that takes a single string (word) as argument. The function mus
 Example
 Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
 */
+/*
 var capitals = function (word) {
   let letter = word.split("");
   let res = [];
@@ -3578,3 +3579,29 @@ var capitals = function (word) {
 };
 
 console.log(capitals("CodEWaRs"));
+*/
+// #3
+/*
+We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
+So given a string "super", we should return a list of [2, 4].
+Some examples:
+Mmmm  => []
+Super => [2,4]
+Apple => [1,5]
+YoMama -> [1,2,4,6]
+NOTES
+    Vowels in this context refers to: a e i o u y (including upper case)
+    This is indexed from [1..n] (not zero indexed!)
+*/
+function vowelIndices(word) {
+  let result = [];
+  let string = word.split("");
+  for (let i = 0; i < string.length; i++) {
+    if (/[aeiouy]/i.test(string[i])) {
+      result.push(i + 1);
+    }
+  }
+  return result;
+}
+
+console.log(vowelIndices("supercalifragilisticexpialidocious"));
