063a87b3f89e35dc559126a0176a81593bd6fbf4
Dmitriy Angve <kirirch129@gmail.com>
2023-06-07 15:28:52 +0300

    calculator

--- script.js
+++ script.js
@@ -3593,6 +3593,7 @@ NOTES
     Vowels in this context refers to: a e i o u y (including upper case)
     This is indexed from [1..n] (not zero indexed!)
 */
+/*
 function vowelIndices(word) {
   let result = [];
   let string = word.split("");
@@ -3605,3 +3606,35 @@ function vowelIndices(word) {
 }
 
 console.log(vowelIndices("supercalifragilisticexpialidocious"));
+*/
+
+// #4
+/*
+You are required to create a simple calculator that returns the result of addition, subtraction, multiplication or division of two numbers
+Your function will accept three arguments:
+The first and second argument should be numbers.
+The third argument should represent a sign indicating the operation to perform on these two numbers.
+if the variables are not numbers or the sign does not belong to the list above a message "unknown value" must be returned.
+Example:
+calculator(1,2,"+"); //=> result will be 3
+calculator(1,2,"&"); //=> result will be "unknown value"
+calculator(1,"k","*"); //=> result will be "unknown value"
+*/
+
+function calculator(a, b, sign) {
+  if (typeof a === "number" && typeof b === "number") {
+    switch (sign) {
+      case "+":
+        return a + b;
+      case "-":
+        return a - b;
+      case "/":
+        return a / b;
+      case "*":
+        return a * b;
+    }
+  }
+  return "unknown value";
+}
+
+console.log(calculator(1, 2, "+"));
