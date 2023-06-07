de1910f19969cb1a99e091ee0088d501ed63ac8b
Dmitriy Angve <kirirch129@gmail.com>
2023-06-07 16:45:01 +0300

    evenNumbers

--- script.js
+++ script.js
@@ -3657,7 +3657,7 @@ countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
 Note
 In case of an empty array return 0. You will not be tested with inval
 */
-
+/*
 function countSmileys(arr) {
   const validSmileys = [
     ":)",
@@ -3684,3 +3684,34 @@ function countSmileys(arr) {
   return count;
 }
 console.log(countSmileys([":D", ":~)", ";~D", ":)"]));
+*/
+
+// #6
+/*
+Given an array of numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+For example:
+([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
+([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
+([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
+*/
+// FIRST SOL
+/*
+function evenNumbers(array, number) {
+  let result = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    if (array[i] % 2 === 0) {
+      result.unshift(array[i]);
+    }
+    if (result.length === number) {
+      break;
+    }
+  }
+  return result;
+}
+
+console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
+*/
+// SEC SOL
+const evenNumbers = (array, number) =>
+  array.filter((item) => item % 2 === 0).slice(-number);
+console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
