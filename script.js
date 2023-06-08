f57fa84809121849ebb41748832d11114fbe94c3
Dmitriy Angve <kirirch129@gmail.com>
2023-06-08 16:31:08 +0300

    order

--- script.js
+++ script.js
@@ -3888,7 +3888,30 @@ n = 0
 m = 1
 result - division by zero (refer to the specifications on how to handle this in your language)
 */
+/*
 function remainder(n, m) {
   return n > m ? n % m : m % n;
 }
 console.log(remainder(17, 5));
+*/
+
+// #4
+/*
+Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result. 
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+Examples
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
+*/
+function order(words) {
+  return words
+    .split(" ")
+    .sort((a, b) => {
+      return a.match(/\d/) - b.match(/\d/);
+    })
+    .join(" ");
+}
+
+console.log(order("is2 Thi1s T4est 3a"));
