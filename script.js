f528c1bafb4f3805efe046f6f5d734ccbe92c3c8
DmitriyAngve <kirirch129@gmail.com>
2023-06-19 13:21:46 +0300

    sumTriangularNumbers

--- script.js
+++ script.js
@@ -4246,6 +4246,7 @@ The sperm cell determines the sex of an individual in this case. If a sperm cell
 Determine if the sex of the offspring will be male or female based on the X or Y chromosome present in the male's sperm.
 If the sperm contains the X chromosome, return "Congratulations! You're going to have a daughter."; If the sperm contains the Y chromosome, return "Congratulations! You're going to have a son.";
 */
+/*
 function chromosomeCheck(sperm) {
   return sperm.includes("Y")
     ? "Congratulations! You're going to have a son."
@@ -4253,3 +4254,33 @@ function chromosomeCheck(sperm) {
 }
 
 console.log(chromosomeCheck("XX"));
+*/
+
+// #4
+
+/*
+Your task is to return the sum of Triangular Numbers up-to-and-including the nth Triangular Number.
+Triangular Number: "any of the series of numbers (1, 3, 6, 10, 15, etc.) obtained by continued summation of the natural numbers 1, 2, 3, 4, 5, etc."
+[01]
+02 [03]
+04 05 [06]
+07 08 09 [10]
+11 12 13 14 [15]
+16 17 18 19 20 [21]
+e.g. If 4 is given: 1 + 3 + 6 + 10 = 20.
+Triangular Numbers cannot be negative so return 0 if a negative number is given.
+*/
+function sumTriangularNumbers(n) {
+  if (n < 0) {
+    return 0;
+  }
+  let sum = 0;
+  let triangle = 0;
+  for (let i = 1; i <= n; i++) {
+    triangle += i;
+    sum += triangle;
+  }
+  return sum;
+}
+
+console.log(sumTriangularNumbers(6));
