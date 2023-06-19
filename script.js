0d37347dde3b1837f40c3681e9b83e6d89061f69
DmitriyAngve <kirirch129@gmail.com>
2023-06-19 13:53:29 +0300

    solve

--- script.js
+++ script.js
@@ -4270,6 +4270,7 @@ Triangular Number: "any of the series of numbers (1, 3, 6, 10, 15, etc.) obtaine
 e.g. If 4 is given: 1 + 3 + 6 + 10 = 20.
 Triangular Numbers cannot be negative so return 0 if a negative number is given.
 */
+/*
 function sumTriangularNumbers(n) {
   if (n < 0) {
     return 0;
@@ -4284,3 +4285,37 @@ function sumTriangularNumbers(n) {
 }
 
 console.log(sumTriangularNumbers(6));
+*/
+
+// #5
+
+/*
+Given a lowercase string that has alphabetic characters only and no spaces, return the highest value of consonant substrings. Consonants are any letters of the alphabet except "aeiou".
+We shall assign the following values: a = 1, b = 2, c = 3, .... z = 26.
+For example, for the word "zodiacs", let's cross out the vowels. We get: "z o d ia cs"
+-- The consonant substrings are: "z", "d" and "cs" and the values are z = 26, d = 4 and cs = 3 + 19 = 22. The highest is 26.
+solve("zodiacs") = 26
+For the word "strength", solve("strength") = 57
+-- The consonant substrings are: "str" and "ngth" with values "str" = 19 + 20 + 18 = 57 and "ngth" = 14 + 7 + 20 + 8 = 49. The highest is 57.
+*/
+function solve(s) {
+  const constants = "bcdfghjklmnpqrstvwxyz";
+  let maxValue = 0;
+  let currentValue = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (constants.includes(s[i])) {
+      currentValue += s.charCodeAt(i) - 96;
+    } else {
+      if (currentValue > maxValue) {
+        maxValue = currentValue;
+      }
+      currentValue = 0;
+    }
+  }
+  if (currentValue > maxValue) {
+    maxValue = currentValue;
+  }
+  return maxValue;
+}
+
+console.log(solve("catchphrase"));
