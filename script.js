da2a61c567319f3b84bf8a0c647a7d6d87559946
DmitriyAngve <kirirch129@gmail.com>
2023-06-19 16:25:41 +0300

    adjacentElementsProduct

--- script.js
+++ script.js
@@ -4347,9 +4347,76 @@ For example
 [99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
 Input sequence contains minimum two elements and every element is an integer.
 */
+/*
 function largestPairSum(numbers) {
   let sorted = numbers.sort((a, b) => b - a);
   return sorted[0] + sorted[1];
 }
 
 console.log(largestPairSum([10, 14, 2, 23, 19]));
+*/
+
+// #8
+
+/*
+Combine strings function
+Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+Example:
+combineNames('James', 'Stevens')
+returns:
+'James Stevens'
+*/
+/*
+function combineNames(s1, s2) {
+  return s1 + " " + s2;
+}
+
+console.log(combineNames("James", "Stevens"));
+*/
+
+// #9
+/*
+Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+Examples (input -> output)
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"
+*/
+/*
+function repeatStr(n, s) {
+  const length = n;
+  const array = [...Array(length).fill(s)];
+  return array.join("");
+  // s.repeat(n)
+}
+console.log(repeatStr(3, "#"));
+*/
+
+// #10
+/*
+Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+Notes
+    Array/list size is at least 2.
+    Array/list numbers could be a mixture of positives, negatives also zeroes.
+Input >> Output Examples
+adjacentElementsProduct([1, 2, 3]); ==> return 6
+Explanation:
+    The maximum product obtained from multiplying 2 * 3 = 6, and they're adjacent numbers in the array.
+    adjacentElementsProduct([9, 5, 10, 2, 24, -1, -48]); ==> return 50
+    Explanation:
+Max product obtained from multiplying 5 * 10  =  50.
+adjacentElementsProduct([-23, 4, -5, 99, -27, 329, -2, 7, -921])  ==>  return -14
+Explanation:
+    The maximum product obtained from multiplying -2 * 7 = -14, and they're adjacent numbers in the array.
+*/
+function adjacentElementsProduct(array) {
+  let max = -Infinity;
+  for (let i = 0; i < array.length - 1; i++) {
+    let current = array[i] * array[i + 1];
+    if (current > max) {
+      max = current;
+    }
+  }
+  return max;
+}
+
+console.log(adjacentElementsProduct([5, 1, 2, 3, 1, 4]));
