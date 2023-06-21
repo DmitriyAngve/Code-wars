a9fa7dba5940695909e7a3edef71875a8324e940
DmitriyAngve <kirirch129@gmail.com>
2023-06-21 13:13:20 +0300

    mergeArrays

--- script.js
+++ script.js
@@ -4596,6 +4596,7 @@ Given an array of numbers, check if any of the numbers are the character codes f
 If they are, change the array value to a string of that vowel.
 Return the resulting array.
 */
+/*
 function isVow(a) {
   for (let i = 0; i < a.length; i++) {
     if (/[aeiou]/.test(String.fromCharCode(a[i]))) {
@@ -4610,3 +4611,51 @@ console.log(
     120, 106,
   ])
 );
+*/
+
+// 21.06
+
+// #1
+/*
+The wide-mouth frog is particularly interested in the eating habits of other creatures.
+
+He just can't stop asking the creatures he encounters what they like to eat. But, then he meets the alligator who just LOVES to eat wide-mouthed frogs!
+
+When he meets the alligator, it then makes a tiny mouth.
+
+Your goal in this kata is to create complete the mouth_size method this method takes one argument animal which corresponds to the animal encountered by the frog. If this one is an alligator (case-insensitive) return small otherwise return wide.
+
+}
+*/
+/*
+function mouthSize(animal) {
+  return animal.toLowerCase() === "alligator" ? "small" : "wide";
+}
+
+console.log(mouthSize("toucan"));
+console.log(mouthSize("alligator"));
+*/
+
+// #2
+/*
+You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
+You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
+Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
+Examples (input -> output)
+* [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+* [1, 3, 5, 7, 9], [10, 8, 6, 4, 2] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+* [1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12] -> [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
+*/
+function mergeArrays(arr1, arr2) {
+  if (arr1 === [] && arr2 === []) {
+    return [];
+  }
+  const newArr = arr1.concat(arr2);
+  const sorted = newArr.sort((a, b) => a - b);
+  const set = [...new Set(sorted)];
+
+  console.log(set);
+  return set;
+}
+
+console.log(mergeArrays([1, 1, 1, 3, 5, 7, 9], [10, 8, 8, 6, 4, 2]));
