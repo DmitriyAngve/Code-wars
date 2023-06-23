7345c874fc4536dc97f859e2760d528917558819
DmitriyAngve <kirirch129@gmail.com>
2023-06-23 13:11:46 +0300

    numberToPower

--- script.js
+++ script.js
@@ -4806,6 +4806,7 @@ solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to
 solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
 solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
 */
+/*
 function solve(s) {
   let countLower = 0;
   let countUpper = 0;
@@ -4826,3 +4827,25 @@ function solve(s) {
 }
 
 console.log(solve("CODe"));
+*/
+
+// #3
+/*
+The goal is to create a function of two inputs number and power, that "raises" the number up to power (ie multiplies number by itself power times).
+Examples
+numberToPower(3, 2)  // -> 9 ( = 3 * 3 )
+numberToPower(2, 3)  // -> 8 ( = 2 * 2 * 2 )
+numberToPower(10, 6) // -> 1000000
+Note: Math.pow and some other Math functions like eval() and ** are disabled.
+*/
+function numberToPower(number, power) {
+  console.info(Math.log2(1024));
+  let result = 1;
+
+  for (let i = 0; i < power; i++) {
+    result *= number;
+  }
+  return result;
+}
+
+console.log(numberToPower(4, 2));
