5d7086e7564444bc996fbfe749dcb4e35859be41
DmitriyAngve <kirirch129@gmail.com>
2023-06-26 12:39:29 +0300

    mango

--- script.js
+++ script.js
@@ -5183,6 +5183,7 @@ Given a positive number n > 1 find the prime factor decomposition of n. The resu
 with the p(i) in increasing order and n(i) empty if n(i) is 1.
 Example: n = 86240 should return "(2**5)(5)(7**2)(11)"
 */
+/*
 function primeFactors(n) {
   const factors = [];
   let factor = 2;
@@ -5212,3 +5213,25 @@ function primeFactors(n) {
 }
 
 console.log(primeFactors(7775460));
+*/
+
+// 26.06
+
+// #1
+
+/*
+There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
+Examples
+mango(2, 3) ==> 6    # 2 mangoes for $3 per unit = $6; no mango for free
+mango(3, 3) ==> 6    # 2 mangoes for $3 per unit = $6; +1 mango for free
+mango(5, 3) ==> 12   # 4 mangoes for $3 per unit = $12; +1 mango for free
+mango(9, 5) ==> 30   # 6 mangoes for $5 per unit = $30; +3 mangoes for free
+*/
+function mango(quantity, price) {
+  const paid = Math.floor(quantity / 3) * 2;
+  const totalCost = paid * price;
+  const free = quantity % 3;
+  return totalCost + free * price;
+}
+
+console.log(mango(3, 3));
