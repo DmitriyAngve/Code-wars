babc084e1beca4fc7c3a613eba3ed94baf86c879
DmitriyAngve <kirirch129@gmail.com>
2023-06-28 13:56:23 +0300

    guessBlue

--- script.js
+++ script.js
@@ -5545,9 +5545,28 @@ console.log(leo(89));
 /*
 Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware.
 */
+/*
 function parseF(s) {
   const parsed = parseFloat(s);
   return isNaN(parsed) ? null : parsed;
 }
 
 console.log(parseF());
+*/
+
+// #4
+/*
+You've decided to write a function, guessBlue() to help automatically calculate whether you should guess "blue" or "red". The function should take four arguments:
+    the number of blue marbles you put in the bag to start
+    the number of red marbles you put in the bag to start
+    the number of blue marbles pulled out so far (always lower than the starting number of blue marbles)
+    the number of red marbles pulled out so far (always lower than the starting number of red marbles)
+guessBlue() should return the probability of drawing a blue marble, expressed as a float. For example, guessBlue(5, 5, 2, 3) should return 0.6.
+*/
+function guessBlue(blueStart, redStart, bluePulled, redPulled) {
+  return (
+    (blueStart - bluePulled) / (blueStart - bluePulled + redStart - redPulled)
+  );
+}
+
+console.log(guessBlue(5, 5, 2, 3));
