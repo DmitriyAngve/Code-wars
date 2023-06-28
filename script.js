368b06b54a246b628c690574737b068d2f731d7a
DmitriyAngve <kirirch129@gmail.com>
2023-06-28 14:01:06 +0300

    cookie

--- script.js
+++ script.js
@@ -5563,6 +5563,9 @@ You've decided to write a function, guessBlue() to help automatically calculate 
     the number of red marbles pulled out so far (always lower than the starting number of red marbles)
 guessBlue() should return the probability of drawing a blue marble, expressed as a float. For example, guessBlue(5, 5, 2, 3) should return 0.6.
 */
+
+// #5
+/*
 function guessBlue(blueStart, redStart, bluePulled, redPulled) {
   return (
     (blueStart - bluePulled) / (blueStart - bluePulled + redStart - redPulled)
@@ -5570,3 +5573,25 @@ function guessBlue(blueStart, redStart, bluePulled, redPulled) {
 }
 
 console.log(guessBlue(5, 5, 2, 3));
+*/
+
+// #6
+/*
+For this problem you must create a program that says who ate the last cookie. If the input is a string then "Zach" ate the cookie. If the input is a float or an int then "Monica" ate the cookie. If the input is anything else "the dog" ate the cookie. The way to return the statement is: "Who ate the last cookie? It was (name)!"
+Ex: Input = "hi" --> Output = "Who ate the last cookie? It was Zach! (The reason you return Zach is because the input is a string)
+Note: Make sure you return the correct message with correct spaces and punctuation.
+Please leave feedback for this kata. Cheers!
+*/
+function cookie(x) {
+  if (typeof x === "string") {
+    return "Who ate the last cookie? It was Zach!";
+  } else if (typeof x === "number") {
+    return "Who ate the last cookie? It was Monica!";
+  }
+  return "Who ate the last cookie? It was the dog!";
+}
+
+console.log(cookie("Ryan"));
+console.log(cookie(26));
+console.log(cookie(2.3));
+console.log(cookie(true));
