e6a978b08f065bb256e614a755d77ca48953c46a
DmitriyAngve <kirirch129@gmail.com>
2023-06-29 15:20:52 +0300

    factorial

--- script.js
+++ script.js
@@ -5862,6 +5862,7 @@ console.log(sortMyString("CodeWars"));
 Given a mixed array of number and string representations of integers, add up the non-string integers and subtract the total of the string integers.
 Return as a number.
 */
+/*
 function divCon(x) {
   let str = 0;
   let num = 0;
@@ -5878,3 +5879,52 @@ function divCon(x) {
 }
 
 console.log(divCon([9, 3, "7", "3"]));
+*/
+
+// #9
+/*
+Strong number is the number that the sum of the factorial of its digits is equal to number itself.
+For example, 145 is strong, since 1! + 4! + 5! = 1 + 24 + 120 = 145.
+Task
+Given a number, Find if it is Strong or not and return either "STRONG!!!!" or "Not Strong !!".
+Notes
+    Number passed is always Positive.
+    Return the result as String
+Input >> Output Examples
+strong_num(1) ==> return "STRONG!!!!"
+Since, the sum of its digits' factorial (1) is equal to number itself, then its a Strong.
+strong_num(123) ==> return "Not Strong !!"
+Since the sum of its digits' factorial of 1! + 2! + 3! = 9 is not equal to number itself, then it's Not Strong.
+strong_num(2)  ==>  return "STRONG!!!!"
+Since the sum of its digits' factorial of 2! = 2 is equal to number itself, then its a Strong.
+strong_num(150) ==> return "Not Strong !!"
+Since the sum of its digits' factorial of 1! + 5! + 0! = 122 is not equal to number itself, Then it's Not Strong . 
+*/
+function strong(n) {
+  function factorial(num) {
+    if (num === 0 || num === 1) {
+      return 1;
+    }
+    let result = 1;
+    for (let i = 2; i <= num; i++) {
+      result *= i;
+    }
+    return result;
+  }
+
+  const arr = n.toString().split("");
+  console.log(arr);
+  let sum = 0;
+
+  arr.forEach(function (num) {
+    sum += factorial(parseInt(num));
+  });
+
+  if (sum === n) {
+    return "STRONG!!!!";
+  } else {
+    return "Not Strong !!";
+  }
+}
+
+console.log(strong(145));
