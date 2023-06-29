176867d256df85d912d7e4f7d3481e16fbcfcda1
DmitriyAngve <kirirch129@gmail.com>
2023-06-29 14:50:11 +0300

    sortMyString

--- script.js
+++ script.js
@@ -5806,6 +5806,7 @@ maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
 Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
 Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
 */
+/*
 var maxSequence = function (arr) {
   let maxSum = 0;
   let currentSum = 0;
@@ -5817,6 +5818,40 @@ var maxSequence = function (arr) {
 };
 
 console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
-// if (array.every((num, ind) => ind === 0 || num >= array[ind - 1])) {
-//   return "yes, ascending";
-// }
+*/
+
+// #7
+/*
+Given a string s. You have to return another string such that even-indexed and odd-indexed characters of s are grouped and groups are space-separated (see sample below) 
+Note: 
+0 is considered to be an even index. 
+All input strings are valid with no spaces
+input: 'CodeWars'
+output 'CdWr oeas'
+S[0] = 'C'
+S[1] = 'o'
+S[2] = 'd'
+S[3] = 'e'
+S[4] = 'W'
+S[5] = 'a'
+S[6] = 'r'
+S[7] = 's'
+Even indices 0, 2, 4, 6, so we have 'CdWr' as the first group
+odd ones are 1, 3, 5, 7, so the second group is 'oeas'
+And the final string to return is 'Cdwr oeas'
+*/
+
+function sortMyString(arr) {
+  let fisrtArr = [];
+  let secondArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2 === 1) {
+      secondArr.push(arr[i]);
+    } else {
+      fisrtArr.push(arr[i]);
+    }
+  }
+  return `${fisrtArr.join("")} ${secondArr.join("")}`;
+}
+
+console.log(sortMyString("CodeWars"));
