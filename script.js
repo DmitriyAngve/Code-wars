38d52eb5092c7f7515ebe90a6b1503d1ad8ef32c
DmitriyAngve <kirirch129@gmail.com>
2023-07-02 19:44:04 +0300

    stringClean

--- script.js
+++ script.js
@@ -6180,18 +6180,58 @@ console.log(uniTotal("aaa"));
 /*
 Write function alternateCase which switch every letter in string from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
 */
+/*
+// FIRST
+// function alternateCase(s) {
+//   let result = "";
+//   for (let i = 0; i < s.length; i++) {
+//     if (s[i].toUpperCase() === s[i]) {
+//       result += s[i].toLowerCase();
+//     } else if (s[i].toLowerCase() === s[i]) {
+//       result += s[i].toUpperCase();
+//     } else {
+//       result += s[i];
+//     }
+//   }
+//   return result;
+// }
+
+// SECOND
 function alternateCase(s) {
-  let result = "";
-  for (let i = 0; i < s.length; i++) {
-    if (s[i].toUpperCase() === s[i]) {
-      result += s[i].toLowerCase();
-    } else if (s[i].toLowerCase() === s[i]) {
-      result += s[i].toUpperCase();
-    } else {
-      result += s[i];
-    }
-  }
-  return result;
+  return s
+    .split("")
+    .map(function (el) {
+      return el === el.toUpperCase() ? el.toLowerCase() : el.toUpperCase();
+    })
+    .join("");
 }
 
 console.log(alternateCase("Hello World"));
+*/
+
+// #3
+/*
+Your boss decided to save money by purchasing some cut-rate optical character recognition software for scanning in the text of old novels to your database. At first it seems to capture words okay, but you quickly notice that it throws in a lot of numbers at random places in the text.
+Examples (input -> output)
+'! !'                 -> '! !'
+'123456789'           -> ''
+'This looks5 grea8t!' -> 'This looks great!'
+Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
+*/
+// FIRST
+// function stringClean(s) {
+//   let result = "";
+//   for (let i = 0; i < s.length; i++) {
+//     if (isNaN(parseInt(s[i]))) {
+//       result += s[i];
+//     }
+//   }
+//   return result;
+// }
+
+// SECOND
+function stringClean(s) {
+  return s.replace(/\d/g, "");
+}
+
+console.log(stringClean("(E3at m2e2!!)"));
