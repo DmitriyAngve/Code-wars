b143103b5a7831fc14133cfe0f2eb7de47fa98d8
DmitriyAngve <kirirch129@gmail.com>
2023-07-05 11:50:51 +0300

    ArrayPrototype

--- script.js
+++ script.js
@@ -6595,7 +6595,7 @@ is_prime(1)   false
 is_prime(2)   true  
 is_prime(-1)  false 
 */
-
+/*
 function isPrime(num) {
   // Задаем условие для поиска простого числа, оно не равно 0, не меньше 0, и не равно 1
   if (num === 0 || num < 0 || num === 1) {
@@ -6616,3 +6616,54 @@ console.log(isPrime(2));
 console.log(isPrime(73));
 console.log(isPrime(-1));
 console.log(isPrime(4));
+*/
+
+// #2
+/*
+This kata is designed to test your ability to extend the functionality of built-in classes. In this case, we want you to extend the built-in Array class with the following methods: square(), cube(), average(), sum(), even() and odd().
+Explanation:
+    square() must return a copy of the array, containing all values squared
+    cube() must return a copy of the array, containing all values cubed
+    average() must return the average of all array values; on an empty array must return NaN (note: the empty array is not tested in Ruby!)
+    sum() must return the sum of all array values
+    even() must return an array of all even numbers
+    odd() must return an array of all odd numbers
+Note: the original array must not be changed in any case!
+Example
+var numbers = [1, 2, 3, 4, 5];
+numbers.square();  // must return [1, 4, 9, 16, 25]
+numbers.cube();    // must return [1, 8, 27, 64, 125]
+numbers.average(); // must return 3
+numbers.sum();     // must return 15
+numbers.even();    // must return [2, 4]
+numbers.odd();     // must return [1, 3, 5]
+*/
+let numbers = [1, 2, 3, 4, 5];
+Array.prototype.square = function () {
+  return this.map((num) => Math.pow(num, 2));
+};
+
+Array.prototype.cube = function () {
+  return this.map((num) => Math.pow(num, 3));
+};
+
+Array.prototype.average = function () {
+  // Вернем 0, если массив пустой
+  if (this.length === 0) {
+    return NaN;
+  }
+  const sum = this.reduce((acc, num) => acc + num, 0);
+  return sum / this.length;
+};
+
+Array.prototype.sum = function () {
+  return this.reduce((acc, num) => acc + num, 0);
+};
+
+Array.prototype.even = function () {
+  return this.filter((num) => num % 2 === 0);
+};
+
+Array.prototype.odd = function () {
+  return this.filter((num) => num % 2 !== 0);
+};
