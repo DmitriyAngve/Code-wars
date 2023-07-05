f4f4e74a45e7a8f9401f444713ac078fa2b0fdd8
DmitriyAngve <kirirch129@gmail.com>
2023-07-05 13:13:20 +0300

    bouncingBall

--- script.js
+++ script.js
@@ -6638,6 +6638,7 @@ numbers.sum();     // must return 15
 numbers.even();    // must return [2, 4]
 numbers.odd();     // must return [1, 3, 5]
 */
+/*
 let numbers = [1, 2, 3, 4, 5];
 Array.prototype.square = function () {
   return this.map((num) => Math.pow(num, 2));
@@ -6667,3 +6668,53 @@ Array.prototype.even = function () {
 Array.prototype.odd = function () {
   return this.filter((num) => num % 2 !== 0);
 };
+*/
+
+// #3
+/*
+Create a method all which takes two params:
+    a sequence
+    a function
+and returns true if the function in the params returns true for every element in the sequence. Otherwise, it should return false. If the sequence is empty, it should return true, since technically nothing failed the test.
+Example
+all((1, 2, 3, 4, 5), greater_than_9) -> false
+all((1, 2, 3, 4, 5), less_than_9)    -> True
+*/
+/*
+function all(arr, fun) {
+  return arr.every(fun);
+}
+*/
+
+// #4
+/*
+A child is playing with a ball on the nth floor of a tall building. The height of this floor above ground level, h, is known.
+He drops the ball out of the window. The ball bounces (for example), to two-thirds of its height (a bounce of 0.66).
+His mother looks out of a window 1.5 meters from the ground.
+How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?
+Three conditions must be met for a valid experiment:
+    Float parameter "h" in meters must be greater than 0
+    Float parameter "bounce" must be greater than 0 and less than 1
+    Float parameter "window" must be less than h.
+If all three conditions above are fulfilled, return a positive integer, otherwise return -1.
+Note:
+The ball can only be seen if the height of the rebounding ball is strictly greater than the window parameter.
+Examples:
+- h = 3, bounce = 0.66, window = 1.5, result is 3
+- h = 3, bounce = 1, window = 1.5, result is -1 
+(Condition 2) not fulfilled).
+*/
+function bouncingBall(h, bounce, window) {
+  if (h <= 0 || bounce <= 0 || bounce >= 1 || window >= h) {
+    return -1;
+  }
+  let count = 1;
+  while (h * bounce > window) {
+    h *= bounce;
+    count += 2;
+  }
+  return count;
+}
+
+console.log(bouncingBall(30.0, 0.66, 1.5));
+console.log(bouncingBall(3.0, 1.0, 1.5));
