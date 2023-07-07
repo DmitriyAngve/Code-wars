e274c068d351586f881a1febace904483e3dfb12
DmitriyAngve <kirirch129@gmail.com>
2023-07-07 15:25:25 +0300

    longestPalindrome

--- script.js
+++ script.js
@@ -7022,6 +7022,7 @@ minSum({9,2,8,7,5,4,0,6}) ==> return (74)
 Explanation:
     The minimum sum obtained from summing each two integers product ,  9*0 + 8*2 +7*4 +6*5 = 74
 */
+/*
 function minSum(arr) {
   arr.sort((a, b) => a - b);
   let left = 0;
@@ -7036,3 +7037,28 @@ function minSum(arr) {
 }
 
 console.log(minSum([5, 4, 2, 3]));
+*/
+
+// #6
+/*
+Longest Palindrome
+Find the length of the longest substring in the given string s that is the same in reverse.
+As an example, if the input was “I like racecars that go fast”, the substring (racecar) length would be 7.
+If the length of the input string is 0, the return value must be 0.
+Example:
+"a" -> 1 
+"aab" -> 2  
+"abcde" -> 1
+"zzbaabcd" -> 4
+"" -> 0
+*/
+function longestPalindrome(s) {
+  if (!s) return 0;
+  for (let c = s.length; c > 0; c--) {
+    for (let i = 0; i <= s.length - c; i++) {
+      let check = s.substr(i, c);
+      if (check === check.split("").reverse().join("")) return c;
+    }
+  }
+}
+console.log(longestPalindrome("baablkj12345432133d"));
