f3fd3327d20285cb5a9dc431587f6091a1ffd047
DmitriyAngve <kirirch129@gmail.com>
2023-07-09 15:49:38 +0300

    createPhoneNumber

--- script.js
+++ script.js
@@ -7281,7 +7281,7 @@ arrayDiff([1,2],[1]) == [2]
 If a value is present in b, all of its occurrences must be removed from the other:
 arrayDiff([1,2,2,2,3],[2]) == [1,3]
 */
-
+/*
 function arrayDiff(a, b) {
   if (a.length === 0 || b.length === 0) {
     return a;
@@ -7290,3 +7290,34 @@ function arrayDiff(a, b) {
 }
 
 console.log(arrayDiff([1, 2, 2, 3, 3], [1, 2]));
+*/
+
+// #6
+/*
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+Example
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+Don't forget the space after the closing parentheses!
+*/
+// FIRST
+// function createPhoneNumber(numbers) {
+//   return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`;
+// }
+
+// console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+
+// SECOND
+// function createPhoneNumber(numbers) {
+//   numbers = numbers.join("");
+//   return (
+//     "(" +
+//     numbers.substring(0, 3) +
+//     ") " +
+//     numbers.substring(3, 6) +
+//     "-" +
+//     numbers.substring(6)
+//   );
+// }
+
+// console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
