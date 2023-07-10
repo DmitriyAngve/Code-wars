d7feb6a1bdd47c68caaaf120b70dc95dd4722bf9
DmitriyAngve <kirirch129@gmail.com>
2023-07-10 19:13:35 +0300

    %3and%5

--- script.js
+++ script.js
@@ -7388,6 +7388,7 @@ So max_rot (or maxRot or ... depending on the language) is such as:
     max_rot(56789) should return 68957
     max_rot(38458215) should return 85821534
 */
+/*
 function max_rot(n) {
   // Преобразуем число в строку
   let number = String(n);
@@ -7405,3 +7406,27 @@ function max_rot(n) {
 }
 
 console.log(max_rot(38458215));
+*/
+
+// #2
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+Note: If the number is a multiple of both 3 and 5, only count it once.
+*/
+function solution(number) {
+  if (number < 0) {
+    return 0;
+  }
+  let count = 0;
+
+  for (let i = 1; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      count += i;
+    }
+  }
+  console.log(count);
+  return count;
+}
+
+console.log(solution(10));
