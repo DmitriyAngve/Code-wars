ee8993c6c06dec0b708372508830024385830ab2
DmitriyAngve <kirirch129@gmail.com>
2023-07-11 13:15:05 +0300

    binaryCount

--- script.js
+++ script.js
@@ -7688,6 +7688,7 @@ Your task is to create a new non-decreasing array that is minimally different fr
 The first entry of the array is correct, and does not require an adjustment.
 The array will never be empty.
 */
+/*
 function cleanedCounts(data) {
   const result = [data[0]];
 
@@ -7702,3 +7703,28 @@ function cleanedCounts(data) {
 }
 
 console.log(cleanedCounts([5, 5, 6, 5, 5, 5, 5, 6]));
+*/
+
+// #7
+/*
+The number n is Evil if it has an even number of 1's in its binary representation.
+The first few Evil numbers: 3, 5, 6, 9, 10, 12, 15, 17, 18, 20
+The number n is Odious if it has an odd number of 1's in its binary representation.
+The first few Odious numbers: 1, 2, 4, 7, 8, 11, 13, 14, 16, 19
+You have to write a function that determine if a number is Evil of Odious, function should return "It's Evil!" in case of evil number and "It's Odious!" in case of odious number.
+good luck :)
+*/
+function evil(n) {
+  let binary = n.toString(2).split("");
+  let count = 0;
+  for (let i = 0; i < binary.length; i++) {
+    if (binary[i] === "1") {
+      count++;
+    }
+  }
+  return count % 2 === 0 ? "It's Evil!" : "It's Odious!";
+}
+
+console.log(evil(1));
+console.log(evil(2));
+console.log(evil(3));
