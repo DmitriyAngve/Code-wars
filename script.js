0e20dcc1f6701e1e4d54d6fec734bb8422b9a504
DmitriyAngve <kirirch129@gmail.com>
2023-07-11 06:58:41 +0300

    trouble

--- script.js
+++ script.js
@@ -7573,6 +7573,7 @@ Example:
 "7"        -> false
 Try to avoid using the % (modulo) operator.
 */
+/*
 function divisibleByThree(str) {
   let arr = str.split("");
   console.log(arr);
@@ -7589,3 +7590,39 @@ function divisibleByThree(str) {
 console.log(divisibleByThree("123"));
 console.log(divisibleByThree("19254"));
 console.log(divisibleByThree("88"));
+*/
+
+// #4
+/*
+Given an array of integers (x), and a target (t), you must find out if any two consecutive numbers in the array sum to t. If so, remove the second number.
+Example:
+x = [1, 2, 3, 4, 5]
+t = 3
+1+2 = t, so remove 2. No other pairs = t, so rest of array remains:
+[1, 3, 4, 5]
+Work through the array from left to right.
+Return the resulting array.
+*/
+// function trouble(x, t) {
+//   let newArr = [];
+//   for (let i = 0; i < x.length; i++) {
+//     if (x[i] + x[i + 1] !== t) {
+//       newArr.push(x[i]);
+//     } else {
+//       newArr.push(x[i]);
+//       i++;
+//     }
+//   }
+//   return newArr;
+// }
+function trouble(x, t) {
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] + x[i + 1] === t) {
+      x.splice(i + 1, 1);
+      i--;
+    }
+  }
+  return x;
+}
+
+console.log(trouble([4, 1, 1, 1, 4], 2));
