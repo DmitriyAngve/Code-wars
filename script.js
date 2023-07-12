c5628f4cefae640ef41719325aabe006a2cdef42
DmitriyAngve <kirirch129@gmail.com>
2023-07-12 06:51:11 +0300

    compareArraysAsString

--- script.js
+++ script.js
@@ -7918,6 +7918,8 @@ The rota should start with Stefan and end with Dee, taking the left-right zigzag
 As an output you would expect in this case:
 ["Stefan", "Raj", "Marie", "Edward", "Amy", "Alexa", "Liz", "Claire", "Juan", "Katie", "Luke", "Dee"])
 */
+// FIRST
+/*
 function binRota(arr) {
   let result = [];
   let rows = arr.length;
@@ -7946,3 +7948,69 @@ console.log(
     ["Dee", "Luke", "Katie"],
   ])
 );
+*/
+
+/*
+// SECOND
+function binRota(arr) {
+  let result = [];
+  // Выбираем второй массив и разворачиваем его
+
+  for (let i = 1; i < arr.length; i = i + 2) {
+    arr[i].reverse();
+  }
+  for (let j = 0; j < arr.length; j++) {
+    result = result.concat(arr[j]);
+  }
+  return result;
+}
+
+console.log(
+  binRota([
+    ["Stefan", "Raj", "Marie"],
+    ["Alexa", "Amy", "Edward"],
+    ["Liz", "Claire", "Juan"],
+    ["Dee", "Luke", "Katie"],
+  ])
+);
+*/
+
+// #2
+/*
+A Tidy number is a number whose digits are in non-decreasing order.
+Task
+Given a number, Find if it is Tidy or not.
+Playing With Numbers Series
+Notes
+    Number passed is always Positive .
+    Return the result as a Boolean
+Input >> Output Examples
+tidyNumber (12) ==> return (true)
+Explanation:
+The number's digits { 1 , 2 } are in non-Decreasing Order (i.e) 1 <= 2.
+tidyNumber (32) ==> return (false)
+Explanation:
+The Number's Digits { 3, 2} are not in non-Decreasing Order (i.e) 3 > 2.
+tidyNumber (1024) ==> return (false)
+Explanation:
+The Number's Digits {1 , 0, 2, 4} are not in non-Decreasing Order as 0 <= 1
+tidyNumber (13579) ==> return (true)
+Explanation:
+The number's digits {1 , 3, 5, 7, 9} are in non-Decreasing Order.
+tidyNumber (2335) ==> return (true)
+Explanation:
+The number's digits {2 , 3, 3, 5} are in non-Decreasing Order , Note 3 <= 3 
+*/
+function tidyNumber(n) {
+  const array = n.toString().split("");
+  const string1 = array.join(",");
+
+  const sortedArray = array.sort((a, b) => a - b);
+  const string2 = sortedArray.join(",");
+
+  return string1 === string2 ? true : false;
+}
+
+console.log(tidyNumber(12));
+console.log(tidyNumber(102));
+console.log(tidyNumber(2789));
