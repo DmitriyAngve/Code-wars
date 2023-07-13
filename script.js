f09765b0da59eac60f537dd86276533404090070
DmitriyAngve <kirirch129@gmail.com>
2023-07-13 17:48:14 +0300

    specialNumber

--- script.js
+++ script.js
@@ -8181,6 +8181,8 @@ console.log(disariumNumber(135));
 */
 
 // 13.07.2023
+
+// #1
 /*
 Jumping number is the number that All adjacent digits in it differ by 1.
 Task
@@ -8213,6 +8215,7 @@ jumpingNumber(32) ==> return "Jumping!!"
 Explanation:
     Adjacent digits differ by 1
 */
+/*
 function jumpingNumber(n) {
   const arr = n.toString().split("");
   console.log(arr);
@@ -8238,3 +8241,52 @@ console.log(jumpingNumber(23));
 console.log(jumpingNumber(32));
 console.log(jumpingNumber(79));
 console.log(jumpingNumber(98789876));
+*/
+
+// #2
+/*
+A number is a Special Number if it’s digits only consist 0, 1, 2, 3, 4 or 5
+Given a number determine if it special number or not .
+    The number passed will be positive (N > 0) .
+    All single-digit numbers within the interval [1:5] are considered as special number.
+Input >> Output Examples
+specialNumber(2) ==> return "Special!!"
+Explanation:
+It's a single-digit number within the interval [1:5] .
+specialNumber(9) ==> return "NOT!!"
+Explanation:
+Although, it's a single-digit number but Outside the interval [1:5] .
+specialNumber(23) ==> return "Special!!"
+Explanation:
+All the number's digits formed from the interval [0:5] digits .
+specialNumber(39) ==> return "NOT!!"
+Explanation:
+Although, there is a digit (3) Within the interval But the second digit is not (Must be ALL The Number's Digits ) .
+specialNumber(59) ==> return "NOT!!"
+Explanation:
+Although, there is a digit (5) Within the interval But the second digit is not (Must be ALL The Number's Digits ) .
+specialNumber(513) ==> return "Special!!"
+specialNumber(709) ==> return "NOT!!"
+*/
+function specialNumber(n) {
+  const arr = n
+    .toString()
+    .split("")
+    .sort((a, b) => b - a);
+  console.log(arr);
+  for (let i = 0; i < arr.length; i++) {
+    let num = parseInt(arr[i]);
+    if (num >= 0 && num <= 5) {
+      return "Special!!";
+    } else {
+      return "NOT!!";
+    }
+  }
+}
+
+console.log(specialNumber(2));
+console.log(specialNumber(6));
+console.log(specialNumber(513));
+console.log(specialNumber(26));
+console.log(specialNumber(25432));
+console.log(specialNumber(65432));
