4d442885b16aa58dd133a3592b0c1ed907510ab6
DmitriyAngve <kirirch129@gmail.com>
2023-07-17 12:57:41 +0300

    outed

--- script.js
+++ script.js
@@ -8735,6 +8735,7 @@ today - The Date object with the date of the check
 cycleLength - Integer representing the length of the cycle in days
 Return true if the number of days passed from last to today is greater than cycleLength. Otherwise, return false.
 */
+/*
 function periodIsLate(last, today, cycleLength) {
   const difInMilSec = today - last;
   const difInDays = difInMilSec / (1000 * 60 * 60 * 24);
@@ -8743,3 +8744,52 @@ function periodIsLate(last, today, cycleLength) {
 }
 
 console.log(periodIsLate());
+*/
+
+// #5
+/*
+Your colleagues have been looking over you shoulder. When you should have been doing your boring real job, you've been using the work computers to smash in endless hours of codewars.
+In a team meeting, a terrible, awful person declares to the group that you aren't working. You're in trouble. You quickly have to gauge the feeling in the room to decide whether or not you should gather your things and leave.
+Given an object (meet) containing team member names as keys, and their happiness rating out of 10 as the value, you need to assess the overall happiness rating of the group. If <= 5, return 'Get Out Now!'. Else return 'Nice Work Champ!'.
+Happiness rating will be total score / number of people in the room.
+Note that your boss is in the room (boss), their score is worth double it's face value (but they are still just one person!).
+*/
+function outed(meet, boss) {
+  let totalScore = 0;
+  let numberOfPeople = Object.keys(meet).length - 1;
+
+  for (let person in meet) {
+    if (person === boss) {
+      totalScore += 2 * meet[person];
+    } else {
+      totalScore += meet[person];
+    }
+  }
+
+  let averageScore = totalScore / (numberOfPeople + 1);
+
+  if (averageScore <= 5) {
+    return "Get Out Now!";
+  } else {
+    return "Nice Work Champ!";
+  }
+}
+
+console.log(
+  outed(
+    {
+      "tim": 0,
+      "jim": 2,
+      "randy": 0,
+      "sandy": 7,
+      "andy": 0,
+      "katie": 5,
+      "laura": 1,
+      "saajid": 2,
+      "alex": 3,
+      "john": 2,
+      "mr": 0,
+    },
+    "laura"
+  )
+);
