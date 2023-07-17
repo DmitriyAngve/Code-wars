03cbed8bfa38b6dd537c9887ac18c48a06150a03
DmitriyAngve <kirirch129@gmail.com>
2023-07-17 06:42:31 +0300

    Person

--- script.js
+++ script.js
@@ -8668,6 +8668,7 @@ explode("102269")
 should return :
 "12222666666999999999"
 */
+/*
 function explode(s) {
   let array = s.split("");
   console.log(array);
@@ -8681,3 +8682,23 @@ function explode(s) {
 console.log(explode("2"));
 console.log(explode("102269"));
 console.log(explode("0"));
+*/
+
+// #2
+/*
+Basic Classes, this kata is mainly aimed at the new JS ES6 Update introducing classes
+Task
+Your task is to complete this Class, the Person class has been created. You must fill in the Constructor method to accept a name as string and an age as number, complete the get Info property and getInfo method/Info getter which should return johns age is 34
+*/
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  get info() {
+    return `${this.name} age is ${this.age}`;
+  }
+}
+
+const person1 = new Person("John", 36);
