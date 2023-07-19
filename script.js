9382f14c55f64f9c6a2d2747c398ef6e97091dc0
DmitriyAngve <kirirch129@gmail.com>
2023-07-19 16:50:13 +0300

    arrayLeaders

--- script.js
+++ script.js
@@ -9035,7 +9035,7 @@ maxProduct ({10, 3, -1, -27} , 3)  return (-30)
 Explanation:
     Since the size (k) equal 3 , then the subsequence of size 3 whose gives product of maxima is 10 * 3 * -1 = -30.
 */
-
+/*
 function maxProduct(numbers, size) {
   let sorted = numbers.sort((a, b) => b - a);
   console.log(sorted);
@@ -9047,3 +9047,61 @@ function maxProduct(numbers, size) {
 }
 
 console.log(maxProduct([-4, -27, -15, -6, -1], 2));
+*/
+
+// #5
+/*
+An element is leader if it is greater than The Sum all the elements to its right side.
+Task
+Given an array/list [] of integers , Find all the LEADERS in the array.
+Notes
+    Array/list size is at least 3 .
+    Array/list's numbers Will be mixture of positives , negatives and zeros
+    Repetition of numbers in the array/list could occur.
+    Returned Array/list should store the leading numbers in the same order in the original array/list .
+Input >> Output Examples
+arrayLeaders ({1, 2, 3, 4, 0}) ==> return {4}
+Explanation:
+    4 is greater than the sum all the elements to its right side
+    Note : The last element 0 is equal to right sum of its elements (abstract zero).
+arrayLeaders ({16, 17, 4, 3, 5, 2}) ==> return {17, 5, 2}
+Explanation:
+    17 is greater than the sum all the elements to its right side
+    5 is greater than the sum all the elements to its right side
+    Note : The last element 2 is greater than the sum of its right elements (abstract zero).
+arrayLeaders ({5, 2, -1}) ==> return {5, 2}
+Explanation:
+    5 is greater than the sum all the elements to its right side
+    2 is greater than the sum all the elements to its right side
+    Note : The last element -1 is less than the sum of its right elements (abstract zero).
+arrayLeaders ({0, -1, -29, 3, 2}) ==> return {0, -1, 3, 2}
+Explanation:
+    0 is greater than the sum all the elements to its right side
+    -1 is greater than the sum all the elements to its right side
+    3 is greater than the sum all the elements to its right side
+    Note : The last element 2 is greater than the sum of its right elements (abstract zero). 
+*/
+/*
+// FIRST
+function arrayLeaders(arr) {
+  const leaders = [];
+  let sum = 0;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] > sum) {
+      leaders.unshift(arr[i]);
+    }
+    sum += arr[i];
+  }
+  return leaders;
+}
+
+console.log(arrayLeaders([1, 2, 3, 4, 0]));
+*/
+
+// SECOND
+const arrayLeaders = (numbers) => {
+  return numbers.filter(
+    (a, i) => numbers.slice(i + 1).reduce((sum, b) => sum + b, 0) < a
+  );
+};
+console.log(arrayLeaders([1, 2, 3, 4, 0]));
