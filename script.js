6d6884528d0390573cdde0c727841297bd755cd4
DmitriyAngve <kirirch129@gmail.com>
2023-07-19 17:17:36 +0300

    maxGap

--- script.js
+++ script.js
@@ -9099,9 +9099,51 @@ console.log(arrayLeaders([1, 2, 3, 4, 0]));
 */
 
 // SECOND
+/*
 const arrayLeaders = (numbers) => {
   return numbers.filter(
     (a, i) => numbers.slice(i + 1).reduce((sum, b) => sum + b, 0) < a
   );
 };
 console.log(arrayLeaders([1, 2, 3, 4, 0]));
+*/
+
+// #6
+/*
+Task
+Given an array/list [] of integers , Find The maximum difference between the successive elements in its sorted form.
+Notes
+    Array/list size is at least 3 .
+    Array/list's numbers Will be mixture of positives and negatives also zeros_
+    Repetition of numbers in the array/list could occur.
+    The Maximum Gap is computed Regardless the sign.
+Input >> Output Examples
+maxGap ({13,10,5,2,9}) ==> return (4)
+Explanation:
+    The Maximum Gap after sorting the array is 4 , The difference between 9 - 5 = 4 .
+maxGap ({-3,-27,-4,-2}) ==> return (23)
+Explanation:
+    The Maximum Gap after sorting the array is 23 , The difference between  |-4- (-27) | = 23 .
+    Note : Regardless the sign of negativity .
+maxGap ({-7,-42,-809,-14,-12}) ==> return (767)  
+Explanation:
+    The Maximum Gap after sorting the array is 767 , The difference between  | -809- (-42) | = 767.
+    Note : Regardless the sign of negativity .
+maxGap ({-54,37,0,64,640,0,-15}) //return (576)
+Explanation:
+    The Maximum Gap after sorting the array is 576 , The difference between  | 64 - 640 | = 576 .
+    Note : Regardless the sign of negativity .
+*/
+function maxGap(numbers) {
+  let sorted = numbers.sort((a, b) => a - b);
+
+  let maxDiff = 0;
+  // i < sorted.length - 1 - это гарантия того, что цикл не выйдет своими итерациями за пределы массива, когда мы обратимся к элементу с индексом sorted[i + 1]
+  for (let i = 0; i < sorted.length - 1; i++) {
+    const difference = Math.abs(sorted[i] - sorted[i + 1]);
+    maxDiff = Math.max(maxDiff, difference);
+  }
+  return maxDiff;
+}
+
+console.log(maxGap([-3, -27, -4, 2]));
