87726eb0e01a2652c22663a0b984c90d76ca943f
DmitriyAngve <kirirch129@gmail.com>
2023-07-21 11:28:27 +0300

    getOrder

--- script.js
+++ script.js
@@ -9134,6 +9134,9 @@ Explanation:
     The Maximum Gap after sorting the array is 576 , The difference between  | 64 - 640 | = 576 .
     Note : Regardless the sign of negativity .
 */
+
+/*
+// FIRST
 function maxGap(numbers) {
   let sorted = numbers.sort((a, b) => a - b);
 
@@ -9147,3 +9150,66 @@ function maxGap(numbers) {
 }
 
 console.log(maxGap([-3, -27, -4, 2]));
+*/
+
+/*
+// SECOND
+function maxGap(arr) {
+  let num = arr.sort((a, b) => a - b);
+  let sum = num[1] - num[0];
+  for (let i = 0; i < num.length; i++) {
+    if (num[i + 1] - num[i] > sum) {
+      sum = num[i + 1] - num[i];
+    }
+  }
+  return sum;
+}
+console.log(maxGap([-3, -27, -4, 2]));
+*/
+
+// 21.07.2023
+
+// #1
+/*
+Some new cashiers started to work at your restaurant.
+They are good at taking orders, but they don't know how to capitalize words, or use a space bar!
+All the orders they create look something like this:
+"milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza"
+The kitchen staff are threatening to quit, because of how difficult it is to read the orders.
+Their preference is to get the orders as a nice clean string with spaces and capitals like so:
+"Burger Fries Chicken Pizza Pizza Pizza Sandwich Milkshake Milkshake Coke"
+The kitchen staff expect the items to be in the same order as they appear in the menu.
+The menu items are fairly simple, there is no overlap in the names of the items:
+1. Burger
+2. Fries
+3. Chicken
+4. Pizza
+5. Sandwich
+6. Onionrings
+7. Milkshake
+8. Coke
+*/
+const menu = [
+  "Burger",
+  "Fries",
+  "Chicken",
+  "Pizza",
+  "Sandwich",
+  "Onionrings",
+  "Milkshake",
+  "Coke",
+];
+const capitalize = (word) => word.slice(0, 1).toUpperCase() + word.slice(1);
+const comparator = (a, b) => menu.indexOf(a) - menu.indexOf(b);
+
+function getOrder(input) {
+  return input
+    .match(new RegExp(menu.join("|"), "ig"))
+    .map(capitalize)
+    .sort(comparator)
+    .join(" ");
+}
+
+console.log(
+  getOrder("milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza")
+);
