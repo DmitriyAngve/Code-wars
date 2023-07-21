ae4259fa474feca0a2f0cfc85efe110542b35847
DmitriyAngve <kirirch129@gmail.com>
2023-07-21 15:07:12 +0300

    trouble2

--- script.js
+++ script.js
@@ -9303,6 +9303,7 @@ Given a two-dimensional array of integers, return the flattened version of the a
 Example:
 Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
 */
+/*
 "use strict";
 function flattenAndSort(array) {
   return [].concat(...array).sort((a, b) => a - b);
@@ -9315,3 +9316,37 @@ console.log(
     [6, 4, 5],
   ])
 );
+*/
+
+// #6
+/*
+Given an array of integers (x), and a target (t), you must find out if any two consecutive numbers in the array sum to t. If so, remove the second number.
+Example:
+x = [1, 2, 3, 4, 5]
+t = 3
+1+2 = t, so remove 2. No other pairs = t, so rest of array remains:
+[1, 3, 4, 5]
+Work through the array from left to right.
+Return the resulting array.
+*/
+function trouble(x, t) {
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] + x[i + 1] === t) {
+      x.splice(i + 1, 1);
+      i--;
+    }
+  }
+  return x;
+}
+
+console.log(trouble([4, 1, 1, 1, 4], 2));
+
+// function trouble(x, t) {
+//   for (let i = 0; i < x.length; i++) {
+//     if (x[i] + x[i + 1] === t) {
+//       x.splice(i + 1, 1);
+//       i--;
+//     }
+//   }
+//   return x;
+// }
