ec61fcb708708e7308843b3582c563611a87d6af
DmitriyAngve <kirirch129@gmail.com>
2023-07-21 11:36:22 +0300

    foldTo

--- script.js
+++ script.js
@@ -9189,6 +9189,7 @@ The menu items are fairly simple, there is no overlap in the names of the items:
 7. Milkshake
 8. Coke
 */
+/*
 const menu = [
   "Burger",
   "Fries",
@@ -9213,3 +9214,30 @@ function getOrder(input) {
 console.log(
   getOrder("milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza")
 );
+*/
+
+// #2
+/*
+Have you heard about the myth that if you fold a paper enough times, you can reach the moon with it? Sure you have, but exactly how many? Maybe it's time to write a program to figure it out.
+
+You know that a piece of paper has a thickness of 0.0001m. Given distance in units of meters, calculate how many times you have to fold the paper to make the paper reach this distance.
+(If you're not familiar with the concept of folding a paper: Each fold doubles its total thickness.)
+
+Note: Of course you can't do half a fold. You should know what this means ;P
+*/
+function foldTo(distance) {
+  if (distance < -0) {
+    return null;
+  }
+  const paperThickness = 0.0001;
+  let folds = 0;
+  let currentThickness = paperThickness;
+
+  while (currentThickness < distance) {
+    currentThickness *= 2;
+    folds++;
+  }
+  return folds;
+}
+
+console.log(foldTo(384000000));
